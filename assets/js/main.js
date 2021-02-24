ba65071b743f7f5e1bf71edc5fb468e729e1aecb
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2021-02-25 10:18:02 +1100

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Arsha - v3.0.3
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
     "use strict";
   
